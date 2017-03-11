50504c2174c563ebe75130fa9a33494b5af02083
Hansen <hansenchen66@gmail.com>
2017-03-11 17:34:50 -0500

    Perfected floor assets according to ULM

--- src/floor_assets/Shelf.java
+++ src/floor_assets/Shelf.java
@@ -1,14 +1,23 @@
 package floor_assets;
-
-
+import java.util.ArrayList;
+/*
+ * Shelves of Aisles
+ */
 public class Shelf{
-	private Rack[] Rack;
-	
+	private ArrayList<Rack> rack;
+	/*
+	 * Initiaze A flexable shelf
+	 */
 	public Shelf(){
-		this.Rack = new Rack[3];
+		this.rack = new ArrayList<Rack>();
 	}
-	
-	public Shelf(int num){
-		this.Rack = new Rack[num];
+	/*
+	 * Part of the auto complete, can be called if needed
+	 */
+	public Shelf(int a,int b){
+		for(int i=0;i<a;i++){
+			Rack x = new Rack(b);
+			this.rack.add(x);
+		}
 	}
 }
