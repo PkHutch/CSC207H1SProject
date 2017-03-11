7c09e58f13640029971d9d7d669f51df6310c13a
ion <ion.barbu@mail.utoronto.ca>
2017-03-11 18:24:08 -0500

    created WareHouse class

--- src/WareHouse.java
+++ src/WareHouse.java
@@ -1,6 +1,84 @@
-
 public class WareHouse {
-	private Worker[] workers;
+	
+	private Floor floor;
+	public FaxMachine fax;
 	private Server server;
-	public boolean ParkerisLate = true;
+	private Worker workers;
+	
+	/*
+	 * Initialize parameters needed to operate a Warehouse 
+	 */
+	
+	public WareHouse(){
+		this.floor = new Floor();
+		this.fax = new FaxMachine();
+		this.server = new Server();
+		this.workers = new Worker();
+		
+	}
+	
+	/*
+	 * Get the fax machine
+	 */
+
+	public String getFaxMachine() {
+		return fax;
+	}
+	
+	/*
+	 * Set a fax for the WareHouse
+	 */
+
+	public void setFaxMachine(FaxMachine fax) {
+		this.fax = fax;
+	}
+	
+	/*
+	 * Get the floor plan of the WareHouse
+	 */
+
+	public Floor getFloor() {
+		return floor;
+	}
+	
+	/*
+	 * Set a floor plan for the WareHouse
+	 */
+	
+	public void setFloor(Floor floor) {
+		this.floor = floor;
+	}
+	
+	/*
+	 * Get the server for the WareHouse
+	 */
+
+	public Server getServer() {
+		return server;
+	}
+	
+	/*
+	 * Set Server for the WareHouse
+	 */
+
+	public void setServer(Server server) {
+		this.server = server;
+	}
+	
+	/*
+	 * Get the workers working in the WareHouse
+	 */
+
+	public Worker getWorkers() {
+		return workers;
+	}
+	
+	/*
+	 * Set the workers for the WareHouse
+	 */
+
+	public void setWorkers(Worker workers) {
+		this.workers = workers;
+	}
+	
 }
