3e5b9c9ff8ecf7fad03f9e2d180f0fe479fc3e2d
Hansen <hansenchen66@gmail.com>
2017-03-11 16:33:15 -0500

    Uploading framwork for sharing

--- src/floor_assets/Floor.java
+++ src/floor_assets/Floor.java
@@ -4,16 +4,31 @@ import java.util.ArrayList;
 public class Floor {
 	public ArrayList<Zone> zones;
 	
+	/*
+	 * Given condition from the handout;
+	 */
 	public Floor(){
 		this.zones = new ArrayList<Zone>();
 	}
-	
+	/*
+	 * Calls for a Floor with a certain number of Zones
+	 */
 	public Floor(int num){
 		this.zones = new ArrayList<Zone>();
 		for (int i=0;i<num;i++){
 			Zone a = new Zone();
 			this.zones.add(a);
-		}
 	}
+	/*
+	 * Auto Completes the Entire area with one array containing the values of each 
+	 * sub zones
+	 */
+	public Floor(a[int]){
+		
+		;
+	
+	}
+	}
+	
 	
 }
