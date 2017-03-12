36130d20bff8daffe7fa81b960d3ed9c5200c1c7
Hansen <hansenchen66@gmail.com>
2017-03-11 19:42:38 -0500

    Added getters and Add/Remove functions for all the floor_asset classes

--- src/floor_assets/Aisle.java
+++ src/floor_assets/Aisle.java
@@ -1,24 +1,53 @@
 package floor_assets;
+
 import java.util.ArrayList;
+
 /*
  * The aisles from a zone
  */
 public class Aisle {
 	private ArrayList<Shelf> shelf;
+
 	/*
 	 * Initialze a flexable shelf for the Aisle
 	 */
-	public Aisle(){
+	public Aisle() {
 		this.shelf = new ArrayList<Shelf>();
 	}
+
 	/*
-	 * Part of the auto-complete function from Floor
-	 * Can be called for specific specs
+	 * Part of the auto-complete function from Floor Can be called for specific
+	 * specs
 	 */
-	public Aisle(int a,int b,int c){
-		for(int i=0;i<a;i++){
-			Shelf x = new Shelf(b,c);
+	public Aisle(int a, int b, int c) {
+		for (int i = 0; i < a; i++) {
+			Shelf x = new Shelf(b, c);
 			this.shelf.add(x);
+		}
+	}
+
+	/*
+	 * Return an ArrayList, the shelf.
+	 */
+	public ArrayList<Shelf> get_Shelves() {
+		return this.shelf;
+	}
+
+	/*
+	 * Add a Shelf s to the arrayList Shelves.
+	 */
+	public void add_shelf(Shelf s) {
+		this.shelf.add(s);
+	}
+
+	/*
+	 * Remove a Shelf s from the arraylist Shelves.
+	 */
+	public void remove_zone(Shelf s) {
+		for (int i = 0; i < this.shelf.size(); i++) {
+			if (this.shelf.get(i) == s) {
+				this.shelf.remove(s);
+			}
 		}
 	}
 }
--- src/floor_assets/Zone.java
+++ src/floor_assets/Zone.java
@@ -1,26 +1,51 @@
 package floor_assets;
+
 import java.util.ArrayList;
+
 /*
  * The zones of a Floor
  */
 public class Zone {
 	private ArrayList<Aisle> aisles;
+
 	/*
 	 * This initize flexable amount of aisles to a zone
 	 */
-	public Zone(){
+	public Zone() {
 		this.aisles = new ArrayList<Aisle>();
 	}
+
 	/*
-	 * Part of the auto-complete function, or can be used for specific
-	 * specs if required.
+	 * Part of the auto-complete function, or can be used for specific specs if
+	 * required.
 	 */
-	public Zone(int a,int b,int c,int d){
+	public Zone(int a, int b, int c, int d) {
 		this.aisles = new ArrayList<Aisle>();
-		for(int i=0;i<a;i++){
-			Aisle y = new Aisle(b,c,d);
+		for (int i = 0; i < a; i++) {
+			Aisle y = new Aisle(b, c, d);
 			this.aisles.add(y);
 		}
 	}
+	/*
+	 * Returns a ArrayList of aisles
+	 */
+	public ArrayList<Aisle> get_aisles() {
+		return this.aisles;
+	}
+	/*
+	 * Add an aisle a into the ArrayList
+	 */
+	public void add_zone(Aisle a) {
+		this.aisles.add(a);
+	}
+	/*
+	 * Remove an Aisle a from the ArrayList
+	 */
+	public void remove_zone(Aisle a) {
+		for (int i = 0; i < this.aisles.size(); i++) {
+			if (this.aisles.get(i) == a) {
+				this.aisles.remove(a);
+			}
+		}
+	}
 }
-
