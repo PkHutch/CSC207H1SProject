6e3b91d699001ac1d2e020e266ba89cb9f75d9d5
ion <ion.barbu@mail.utoronto.ca>
2017-03-11 19:29:53 -0500

    created worker class

--- src/Worker.java
+++ src/Worker.java
@@ -1,4 +1,29 @@
+import java.util.Arrays;
 
-public class Worker {
+public abstract class Worker {
+	
+	private String name;
+	
+	/*
+	 * Initialize parameter needed for each worker
+	 */
+	
+	public Worker(){
+		this.name = name;
+	}
+	
+	/*
+	 * get name of a worker
+	 */
 
+	public String getName() {
+		return name;
+	}
+	
+	/*
+	 * abstract method that is implemented differently based on worker description
+	 */
+	
+	abstract void doTask();
+	
 }
