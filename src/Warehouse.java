050a20a53a736abdc454ba25ea5b1da412f57a84
PkHutch <pk-hutch@hotmail.com>
2017-03-11 23:33:51 -0500

    Fix the conflict in the Warehouse file.
    Fix the conflict, but also edit the file for style.

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,7 +1,55 @@
+import java.util.Arrays;
 import workers.Worker;
+import floor_assets.*;
 
+/**
+ * The warehouse class, which is where the simulation takes place.
+ */
 public class Warehouse {
-	private Worker[] workers;
-	private Server server;
-	public boolean ParkerisLate = true;
+    private ArrayList<FaxMachine> faxMachines;
+    private Floor floor;
+    private Server server;
+    private ArrayList<Worker> workers;
+
+    /**
+     * Initializes a Warehouse object.
+     */
+    public Warehouse() {
+        this.faxMachines = new ArrayList<FaxMachine>;
+        this.floor = new Floor();
+        this.server = new Server();
+        this.workers = new Worker();
+    }
+
+    /**
+     * Returns the fax machines of the Warehouse.
+     * @return Returns the ArrayList of FaxMachine.
+     */
+    public ArrayList<FaxMachine> getFaxMachines() {
+        return this.faxMachines;
+    }
+
+    /**
+     * Return the floor object of the warehouse.
+     * @return Returns the object which is the floor attribute.
+     */
+    public Floor getFloor() {
+        return floor;
+    }
+
+    /**
+     * Returns the server, which makes the decisions for the warehouse.
+     * @return Returns the server object for the warehouse.
+     */
+    public Server getServer() {
+        return server;
+    }
+
+    /**
+     * Returns the workers, which do tasks within the warehouse.
+     * @return Returns the workers of the warehouse.
+     */
+    public Worker getWorkers() {
+        return workers;
+    }
 }
