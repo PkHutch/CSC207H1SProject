67ceafa49886b8ce0c3db35b3986c27636d0c033
PkHutch <pk-hutch@hotmail.com>
2017-03-15 00:09:57 -0400

    issue-67: Add to src/Server.java so that it the file is included in the entities package.

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,6 @@
+// Defines the package.
+package entities;
+
 /**
  * This class defines the Server, which makes the majority of the logical decisions for a
  * warehouse. The specifics of the logical decisions that it makes are located in the
