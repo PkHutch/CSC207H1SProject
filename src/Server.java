9f0b3f1b84d28fb0f3280985763abbd6ee781e01
PkHutch <pk-hutch@hotmail.com>
2017-03-15 00:51:16 -0400

    issue-67: Edit src/Server.java so that the IllegalArgumentException is imported and not the IllegalFormatException, fixing an error.

--- src/Server.java
+++ src/Server.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.IllegalFormatException;
+import java.util.IllegalArgumentException;
 
 /**
  * This class defines the Server, which makes the majority of the logical decisions for a
