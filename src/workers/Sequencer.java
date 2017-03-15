fcd5bfa117348da2233bbb08df1260d2a5e266b6
ion <ion.barbu@mail.utoronto.ca>
2017-03-15 01:14:28 -0400

    added worker changes and vehicle

--- src/workers/Sequencer.java
+++ src/workers/Sequencer.java
@@ -2,8 +2,8 @@ package workers;
 
 public class Sequencer extends Worker {
 
-	public Sequencer(String name) {
-		super(name);
+	public Sequencer(String name, Warehouse warehouse) {
+		super(name, warehouse);
 	}
 
 	public void doTask(String location) {
