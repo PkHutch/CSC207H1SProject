75710ebd108ce40d659516fb5bf14e3121510d42
PkHutch <pk-hutch@hotmail.com>
2017-03-15 00:08:35 -0400

    issue-67: Add to src/Server.java so that there exists class documentation for the Server class.

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,8 @@
+/**
+ * This class defines the Server, which makes the majority of the logical decisions for a
+ * warehouse. The specifics of the logical decisions that it makes are located in the
+ * issueTask methods.
+ */
 public class Server {
     // Filler.
 }
