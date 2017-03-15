c656648909cd7f34469e5101aaf649c0853ad2fe
PkHutch <pk-hutch@hotmail.com>
2017-03-15 06:16:15 -0400

    issue-67: Add work on Picker.java so far.

--- src/entities/workers/Picker.java
+++ src/entities/workers/Picker.java
@@ -1,39 +1,41 @@
+// Defines the package.
 package entities.workers;
 
-import java.util.*;
-import entities.Worker;
-import entities.vehicles.Forklift;
+// Defines the imports.
+import entities.Level;
 import entities.Stock;
+import entities.vehicles.Forklift;
 import entities.Warehouse;
+import entities.Worker;
 
-/*
- * The Picker class, a worker in the warehouse
+/**
+ * The Picker class extends the worker class and takes an arguement of String for it's doTask
+ * method. The Pickers job to stock off the shelves and bring them to the marshalling area as
+ * per the server's instructions.
  */
-
 public class Picker extends Worker<String> {
-	
-	private Stack<Stock> stack;
-	private Forklift forklift;
+    private Forklift forklift;
 
-	public Picker(String name, Warehouse warehouse) {
-		super(name, warehouse);
-		this.forklift = new Forklift();
-	}
+    public Picker(String name, Warehouse warehouse) {
+        super(name, warehouse);
+        this.forklift = new Forklift();
+    }
 
-	public void doTask(String location) {
-		String[] result = location.split(",");
-		char c = result[0].charAt(0);
-		int[] A = new int[5];
-		A[0] = 0;
-		for (int j = 1; j < 5; j++) {
-			A[j] = Integer.parseInt(result[j]);
-		}
-		this.getWarehouse().getFloor().getZone(result[0].charAt(0)).getAisle(Integer.parseInt(result[1])).getRack(Integer.parseInt(result[2])).getLevel(Integer.parseInt(result[3])).removeItem(new Stock(Integer.parseInt(result[4])));
-		forklift.addItem(new Stock(A[4]));
-		stack.push(new Stock(A[4]));
-		if(warehouse.floor.getZone()[c].getAisle()[A[1]].getRacks()[A[2]].getLevel()[A[3]].numItem <= 5) {
-			throw new EmptyStackException();
-		}
-	}
-
+    public void doTask(String argument) {
+        if(argument.equals("ready")) {
+                String[] result = location.split(",");
+                char c = result[0].charAt(0);
+                int[] A = new int[5];
+                A[0] = 0;
+                for (int j = 1; j < 5; j++) {
+                    A[j] = Integer.parseInt(result[j]);
+                }
+                Level currentLevel = this.getWarehouse().getFloor().getZone(result[0].charAt(0)).getAisle(Integer.parseInt(result[1])).getRack(Integer.parseInt(result[2])).getLevel(Integer.parseInt(result[3]));
+                Stock currentStock = currentLevel.removeItem(Integer.parseInt(result[4]));
+                forklift.addItem(currentStock);
+                if(currentLevel.numItem() <= 5) {
+                    this.getWarehouse().getServer().issueTask(currentLevel);
+                }
+        }
+    }
 }
