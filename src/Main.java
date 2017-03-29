78f3aec387d1d96996b1f4af3b26c64e209eb400
Hansen <hansenchen66@gmail.com>
2017-03-29 04:50:00 -0400

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,256 @@
+
+// Defines imports.
+import entities.Level;
+import entities.Warehouse;
+import entities.arraylistcontainers.Floor;
+import entitycommands.EntityCommand;
+import entitycommands.OrderCommand;
+import entitycommands.workercommands.PickerCommand;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * The main running class.
  */
 public class Main {
-    // Defines instance variables.
-    private static final String QUIT_COMMAND = "Quit";
+	// Defines instance variables.
+	private static final String QUIT_COMMAND = "Quit";
+	private static final String SAVE_FILE = "./resources/final.csv";
+	private static final String INITIAL_FILE = "./resources/traversal_table.csv";
 
-    // Defines the main method.
-    /**
-     * The main method, if the project is finished, this will have the text input, and the actions
-     * taken upon input. It should loop until the quit command is given, defined by the constant,
-     * otherwise it should execute the given command if it is valid.
-     *
-     * @param args Unused parameter required in main.
-     */
-    public static void main(String[] args) {
-        System.out.println("Calling main.");
-        // This is already implemented, and will be included in a different pull request, do not
-        // impelement this.
-    }
+	/**
+	 * The main method, if the project is finished, this will have the text
+	 * input, and the actions taken upon input. It should loop until the quit
+	 * command is given, defined by the instance variable, otherwise it should
+	 * execute the given command in the console.
+	 *
+	 * @param args
+	 *            Unused parameter required in main.
+	 */
+	public static void main(String[] args) {
+		Warehouse warehouse = new Warehouse();
+		EntityCommand[] commands = new EntityCommand[] { new OrderCommand(warehouse), new PickerCommand(warehouse) };
+		loadInitialState(warehouse.getFloor());
+		// Defines the variables that need to be used for user input.
+		String currentInput = "";
+		Scanner inputScanner = new Scanner(System.in);
+		// Keeps getting user input and inputting the commands until the quit
+		// command is given.
+		while (!(currentInput.equals(QUIT_COMMAND))) {
+			// Gets user input.
+			System.out.print("Input: ");
+			currentInput = inputScanner.nextLine();
 
-    // Defines the helper methods.
-    /**
-     * The method for saving final.csv, which checks every level in a warehouse and writes the
-     * levels into the function that do not include those at max capacity. The format will be as
-     * follows: each level will be written once to final.csv, each level will exist on a new line,
-     * in the format of "zone,aisle,rack,level,quantity", where "zone" is a character, and the
-     * remaining variables are the respective integers.
-     *
-     * @param floor the Floor which should be utilized in the creation of final.csv.
-     */
-    private static void saveFinalState(Floor floor) {
-        System.out.println("Calling saveFinalState of Main.");
-        // Check every Level, calling, floor.getLevels() to get the levels contained in the Floor.
-        // Then check each Level for whether or not it is at max capacity, if it is not
-        // hen get the path to the Level by calling, getLocation() on the level.
-        // Then write the resulting new line to the file if the previous was true.
-    }
+			// Sets up for checking commands.
+			boolean commandFound = false;
 
-    /**
-     * The method for loading from initial.csv, which takes every level from the file, and loads
-     * it with the appropriate quantity of stock, according to the input of initial.csv, which
-     * should be written in the following format: each level is written in once, and on a new line
-     * in the format of "zone,aisle,rack,level,quantity", where "zone" is a character, and the
-     * remaining variables are the respective integers.
-     *
-     * @param floor the Floor which should have each Level filled with the correct amount of
-     *        Stock.
-     */
-    private static void loadInitialState(Floor floor) {
-        System.out.println("Calling loadInitialState of Main.");
-        // Parse initial.csv into an ArrayList, with each line being an element.
-        // Ensure that there are no repeats of levels, that is that each line doesn't
-        // start with the same String excluding the quantity, if it does, throw an exception.
-        // If it doesn't then add it inot the parsed ArrayList.
-        // Then take the parsed ArrayList and turn it into an array.
-        // Then use sort from the Arrays class to turn it into a sorted array.
-        // Then create an int nonMaxLevelIndex, and set it to zero.
-        // Then for each Level of the floor, check if the path is the same as that given by
-        // nonMaxLevelIndex and the sorted array, if it is, use that quantity and add one to
-        // nonMaxLevelIndex, if it is not, then set the quantity to the max.
-        // make sure the quantity given to the level is not larger than the max.
-        // Go until all Level are filled.
-    }
+			System.out.println("    Checking if the input is a valid command.");
+			// Keep checking until there is a valid command or all commands have
+			// been checked.
+			for (int index = 0; (!(commandFound) && index < commands.length); index++) {
+				System.out.println("    Currently checking commands[" + Integer.toString(index) + "].");
+				String commandString = commands[index].getCommand();
+
+				System.out.println("    Checking if " + currentInput + " starts with " + commandString);
+				// If the command is valid, execute the command.
+				if (currentInput.startsWith(commandString)) {
+					try {
+						saveFinalState(warehouse.getFloor());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					commandFound = true;
+					String argument = currentInput.substring(commandString.length() + 1);
+					commands[index].executeCommand(argument);
+					try {
+						saveFinalState(warehouse.getFloor());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				} else {
+					index++;
+				}
+			}
+		}
+		//closes the Scanner.
+		inputScanner.close();
+
+		// Saves the final state into final.csv
+		try {
+			saveFinalState(warehouse.getFloor());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * The method for saving final.csv, . . . . .
+	 */
+	private static void saveFinalState(Floor floor) throws IOException {
+		// Check every level, calling, floor.getItems() to get the floor.
+		// Then get each level, by calling getLevel() on floor.
+		// Then check if the Level is at max capacity, if it is
+		// Then get each path to the level by calling, getLocation() on the
+		// level.
+		// Then write the resulting new line to the file if the line exists.
+		System.out.println("The current state of warehouse has been saved");
+		String[] floors = floor.getLevels();
+		//goes through each possible location in the warehouse
+		for (int i = 0; i < floors.length; i++) {
+			//gets a specific location according to the String location
+			//provided by String[] floors
+			Level level = floor.getLevel(
+					floors[i].charAt(0), 
+					Integer.parseInt(floors[i].substring(2, 3)),
+					Integer.parseInt(floors[i].substring(4, 5)), 
+					Integer.parseInt(floors[i].substring(6, 7)));
+			if (!level.atMaxCapacity()) {
+				//writes to the file
+				writeFile(floors[i]);
+			}
+		}
+	}
+
+	private static void writeFile(String content) {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(SAVE_FILE, true))) {
+			bw.write(content);
+			bw.newLine();
+			System.out.println("Wrote current state to to final.csv");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private static String[] checkForDup(){
+		String line;
+		ArrayList<String> initFile = new ArrayList<String>();
+		boolean bool = true;
+		try {
+			// FileReader reads text files in the default encoding.
+			FileReader fileReader = new FileReader(INITIAL_FILE);
+
+			// Always wrap FileReader in BufferedReader.
+			BufferedReader br = new BufferedReader(fileReader);
+			// Appends the line of location into an Arraylist.
+			while ((line = br.readLine()) != null) {
+				initFile.add(line);
+			}
+			// closes the file.
+			br.close();
+			}catch (FileNotFoundException ex) {
+				System.out.println("Unable to open file '" + INITIAL_FILE + "'");
+			} catch (IOException ex) {
+				System.out.println("Error reading file '" + INITIAL_FILE + "'");
+				ex.printStackTrace();
+			}
+			//converts the Arraylist to an array to provide efficiency. 
+			String[] sortedInitFile = (String[]) initFile.toArray();
+			//sorts the array for maximum search efficiency
+			Arrays.sort(sortedInitFile);
+			for(int i=0;i<sortedInitFile.length-1;i++){
+				//compares the i'th index location to the i+1'th index location
+				//to check for duplicates since they are sorted
+				//if they are duplicates they must be adjacent to each other.
+				if(sortedInitFile[i] == sortedInitFile[i+1]){
+					System.out.println("There exists a duplicate in Floor location");
+					//fails the dup test and should return null and notify the user
+					//this initializing has failed.
+					bool = false;
+				}
+			}
+			//returns the array of locations if no duplicates
+			if (bool==true){
+				return sortedInitFile;
+			//returns null for now, should raise exception later.
+			}else{
+				return null;
+			}
+
+	}
+	//Adderall code, if remove, fix loadInitialState according.
+	//this pops an element of the array
+	//HINT: Read loadInitialState for further instruction.
+	private static void removeElement(String[]a,int del){
+		System.arraycopy(a, del+1, a, del, a.length-1-del);
+	}
+	
+	
+	private static void loadInitialState(Floor floor) {
+		System.out.println("Loading the initial state of the warehouse");
+		// Parse initial.csv into an ArrayList, with each line being an element.
+		// This will reference one line at a time
+		String[] init = checkForDup();
+		String[] floors = floor.getLevels().clone();
+		//HINT: Change these to non-final variables if we do not pop them
+		final int POINTERA = 0;
+		final int POINTERB = 0;
+		if (init != null){
+			//HINT: 
+			while (init.length != 0 && floor.getLevels().length != 0){
+				//the case where both array are still active, we would
+				//compare to two and decide how to fill it
+				if(init.length!=0 || floor.getLevels().length!=0){
+					//if The current pointed Floor location is lexicographically 
+					//greater than the init location, we fill it to the MAX
+					if(init[POINTERA].compareTo(floors[POINTERB])<0){
+						String location = init[POINTERA];
+						char zone = location.charAt(0);
+						int aisle = Integer.parseInt(location.substring(2, 3));					
+						int rack  = Integer.parseInt(location.substring(4, 5));
+						int level = Integer.parseInt(location.substring(6, 7));
+						Level theLevel = floor.getLevel(zone,aisle,rack,level);
+						theLevel.addStock(Integer.parseInt(location.substring(8, 9)));
+						theLevel.addStock(theLevel.getMaxCapacity());
+						//pops from level(x)
+						//HINT: change to pointerA += 1 
+						removeElement(floors,POINTERB);
+					//if the current location is equal to the init location
+					//lexicographically, we add the appropriate amount to it
+					//which should be stored at location.substring(8,9)
+					}else if(init[POINTERA].compareTo(floors[POINTERB])==0){
+						String location = init[POINTERA];
+						char zone = location.charAt(0);
+						int aisle = Integer.parseInt(location.substring(2, 3));					
+						int rack  = Integer.parseInt(location.substring(4, 5));
+						int level = Integer.parseInt(location.substring(6, 7));
+						Level theLevel = floor.getLevel(zone,aisle,rack,level);
+						theLevel.addStock(Integer.parseInt(location.substring(8, 9)));
+						//pops both locations for further searching.
+						//HINT: change to pointerA+=1 and pointerB+=1
+						removeElement(init,POINTERA);
+						removeElement(floors,POINTERB);
+					 //if current pointed Floor location is lexicographically
+					 //less than the init location, it shouldn't happen
+					 //So we give an Error message and essentially quit.
+					}else{
+						System.out.println("ERROR: This Poisition does not exist on the floor");
+						System.out.println("Please check the files for duplicates or non-existant");
+						System.out.println("locations");
+						//Raise an exception here
+						break;
+					}
+				//if init.csv runs out of lines. We fill the rest to max Stock
+				}else if(init.length==0 && floor.getLevels().length!=0){
+						String location = init[POINTERA];
+						char zone = location.charAt(0);
+						int aisle = Integer.parseInt(location.substring(2, 3));					
+						int rack  = Integer.parseInt(location.substring(4, 5));
+						int level = Integer.parseInt(location.substring(6, 7));
+						Level theLevel = floor.getLevel(zone,aisle,rack,level);
+						theLevel.addStock(Integer.parseInt(location.substring(8, 9)));
+						theLevel.addStock(theLevel.getMaxCapacity());
+						//Change to pointerB += 1 here
+						removeElement(floors,POINTERB);	
+				}
+			}
+		}
+	}
 }
