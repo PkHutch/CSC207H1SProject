85527307d6ea6ce1a71bb699770c7b0bb2c1db5a
PkHutch <pk-hutch@hotmail.com>
2017-03-29 07:19:41 -0400

    issue-102: Add src/entities/Level.java such that the initial implementation is implemented.

---
+++ src/entities/Level.java
@@ -0,0 +1,33 @@
+// Defines the package.
+package entities;
+
+// Defines the imports.
+import entities.arraylistcontainers.Rack;
+
+/**
+ * The Level class contains Stock of a single SKU and is contained by a Rack.
+ */
+public class Level {
+    // Defines the constants.
+    private static final int DEFAULT_MAX_QUANTITY = 30;
+
+    // Defines the instance variables.
+    private Rack containedBy;
+    private Integer sku;
+
+    // Defines the constructors.
+    /**
+     * The default Level constructor, constructs the Level according to the DEFAULT_MAX_QUANTITY
+     * for assigning the currentQuantity because no quantity has been given.
+     *
+     * @param rack the Rack object which contains this Level.
+     * @param sku the Integer which this Level contains, this should be a valid sku as defined by
+     *        the Server that the Level belongs to, but this is not inforced.
+     */
+    public Level(Rack rack, Integer sku) {
+        System.out.println("Constructing Level " + this.toString() + " with argument rack as " +
+            rack.toString() + ".");
+        this.containedBy = rack;
+        this.sku = sku;
+    }
+}
