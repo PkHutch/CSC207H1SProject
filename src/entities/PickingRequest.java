fbdd0e2238c2df91e306b55491ad5b41c03673f6
Hansen <hansenchen66@gmail.com>
2017-04-01 04:21:25 -0400

    Modified compatability

--- src/entities/PickingRequest.java
+++ src/entities/PickingRequest.java
@@ -49,7 +49,7 @@ public class PickingRequest {
      */
     public int getStatus() {
         System.out.println("Calling getStatus of PickingRequest " + this.toString() + ".");
-        System.out.println("    Returning " + this.status.toString() + ".");
+        System.out.println("    Returning " + this.status + ".");
         return status;
     }
 
@@ -63,7 +63,7 @@ public class PickingRequest {
      */
     public void setStatus(int status) {
         System.out.println("Calling setStatus of PickingRequest " + this.toString() + ", " +
-            "where argument status as " + status.toString() + ".");
+            "where argument status as " + this.status + ".");
         this.status = status;
     }
 }
