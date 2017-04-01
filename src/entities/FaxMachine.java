cdd024dabc6101f1e063d4a442990eb87ce7448e
Hansen <hansenchen66@gmail.com>
2017-04-01 14:11:28 -0400

    Phase 2 functionality completed

--- src/entities/FaxMachine.java
+++ src/entities/FaxMachine.java
@@ -5,25 +5,30 @@ package entities;
  * The FaxMachine class is used to store faxes, which are single orders.
  */
 public class FaxMachine {
-    // Defines the contructor methods.
-    /**
-     * The main constructor for a FaxMachine.
-     *
-     * @param server the Server that the fax machine belongs to.
-     */
-    public FaxMachine(Server server) {
-        // Don't worry about this.
-    }
+	private Server server;
 
-    // Defines the functional methods.
-    /**
-     * The addOrder method adds an order to the FaxMachines current orders, as of now, it also
-     * updates the server with the order and never even adds it to the orders property of a fax
-     * machine.
-     *
-     * @param order the Order to be added to the FaxMachine.
-     */
-    public void addOrder(Order order) {
-        // Don't worry about this.
-    }
+	// Defines the contructor methods.
+	/**
+	 * The main constructor for a FaxMachine.
+	 *
+	 * @param server
+	 *            the Server that the fax machine belongs to.
+	 */
+	public FaxMachine(Server server) {
+		this.server = server;
+	}
+
+	// Defines the functional methods.
+	/**
+	 * The addOrder method adds an order to the FaxMachines current orders, as
+	 * of now, it also updates the server with the order and never even adds it
+	 * to the orders property of a fax machine.
+	 *
+	 * @param order
+	 *            the Order to be added to the FaxMachine.
+	 */
+	public void addOrder(Order order) {
+		System.out.println("Added an order to the Server.");
+		this.server.addOrder(order);
+	}
 }
