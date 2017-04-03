fb22a40f5eac82675662be2c5146b345c450bab1
PkHutch <pk-hutch@hotmail.com>
2017-04-02 23:47:44 -0300

    issue-175: Add all necessary changes for ready implementation.

--- src/entities/workers/Sequencer.java
+++ src/entities/workers/Sequencer.java
@@ -7,93 +7,97 @@ import entities.PickingRequest;
 import entities.Stock;
 import entities.taskentities.TaskGiver;
 import entities.Warehouse;
+import java.lang.IllegalStateException;
 import java.util.ArrayList;
 
 public class Sequencer extends Worker implements TaskGiver {
+    private boolean isActive;
+
     public Sequencer(String name, Warehouse warehouse) {
         super(name, warehouse);
+        this.isActive = true;
     }
 
     public void doTask() {
-        ArrayList<PickingRequest> pickingRequests = 
-            this.getWarehouse().getServer().getPickingRequests();
-        Marshalling marshalling = this.getWarehouse().getMarshalling();
-        ArrayList<Stock> stock = marshalling.getMarshallingStock();
-        for(int pickingRequestIndex = 0; pickingRequestIndex < pickingRequests.size();
-            pickingRequestIndex++) {
-            if(pickingRequests.get(pickingRequestIndex).getStatus() == 2) {
-                PickingRequest potentialPickingRequest = pickingRequests.get(pickingRequestIndex);
-                Integer[] potentiallySequenceableSKUs = potentialPickingRequest.getSKUs();
-                ArrayList<Stock> potentiallySequenceableStock = new ArrayList<>();
-                for(int skuIndex = 0; skuIndex < potentiallySequenceableSKUs.length; skuIndex++) {
+        if(this.isActive == false) {
+            ArrayList<PickingRequest> pickingRequests = 
+                                          this.getWarehouse().getServer().getPickingRequests();
+            Marshalling marshalling = this.getWarehouse().getMarshalling();
+            ArrayList<Stock> stock = marshalling.getMarshallingStock();
+            for(int pickingRequestIndex = 0; pickingRequestIndex < pickingRequests.size();
+                pickingRequestIndex++) {
+                if(pickingRequests.get(pickingRequestIndex).getStatus() == 2) {
+                    PickingRequest potentialPickingRequest = pickingRequests.get(
+                                                                 pickingRequestIndex);
+                    Integer[] potentiallySequenceableSKUs = potentialPickingRequest.getSKUs();
+                    ArrayList<Stock> potentiallySequenceableStock = new ArrayList<>();
+                    for(int skuIndex = 0; skuIndex < potentiallySequenceableSKUs.length;
+                        skuIndex++) {
+                        boolean stockFound = false;
+                        for(int stockIndex = 0; stockIndex < stock.size() && stockFound == false;
+                            stockIndex++) {
+                            if(potentiallySequenceableSKUs[skuIndex].equals(stock.get(
+                                stockIndex).getSKU())) {
+                                stockFound = true;
+                                potentiallySequenceableStock.add(marshalling.popStock(
+                                    stockIndex));
+                            }
+                        }
+                    }
+                    if(potentiallySequenceableStock.size() == 
+                        potentiallySequenceableSKUs.length) {
+                        marshalling.dumpSequenceableStock(potentiallySequenceableStock);
+                        potentialPickingRequest.setStatus(3);
+                    } else {
+                        marshalling.dumpStock(potentiallySequenceableStock);
+                        potentialPickingRequest.setStatus(0);
+                    }
+                }
+            }
+            marshalling.clearStock();
+            while(pickingRequests.size() > 0 && pickingRequests.get(0).getStatus() == 3) {
+                ArrayList<Stock> backPalletArrayList = new ArrayList<>();
+                ArrayList<Stock> frontPalletArrayList = new ArrayList<>();
+                Integer[] sequenceableSKUs = pickingRequests.remove(0).getSKUs();
+                boolean loadToFront = true;
+                ArrayList<Stock> sequenceableStock = marshalling.getSequenceableStock();
+                for(int skuIndex = 0; skuIndex < sequenceableSKUs.length; skuIndex++) {
                     boolean stockFound = false;
-                    for(int stockIndex = 0; stockIndex < stock.size() && stockFound == false;
-                    stockIndex++) {
-                        if(potentiallySequenceableSKUs[skuIndex].equals(
-                            stock.get(stockIndex).getSKU())) {
+                    for(int stockIndex = 0; stockIndex < sequenceableStock.size() &&
+                        stockFound == false; stockIndex++) {
+                        if(sequenceableStock.get(stockIndex).getSKU().equals(sequenceableSKUs[
+                            skuIndex])) {
                             stockFound = true;
-                            potentiallySequenceableStock.add(marshalling.popStock(stockIndex));
+                            if(loadToFront == true) {
+                                loadToFront = false;
+                                frontPalletArrayList.add(marshalling.popSequenceableStock(
+                                    stockIndex));
+                            } else {
+                                loadToFront = true;
+                                backPalletArrayList.add(marshalling.popSequenceableStock(
+                                    stockIndex));
+                            }
                         }
                     }
                 }
-                if(potentiallySequenceableStock.size() == potentiallySequenceableSKUs.length) {
-                    marshalling.dumpSequenceableStock(potentiallySequenceableStock);
-                    potentialPickingRequest.setStatus(3);
-                } else {
-                    marshalling.dumpStock(potentiallySequenceableStock);
-                    potentialPickingRequest.setStatus(0);
-                }
+                marshalling.addPallet(new Pallet(frontPalletArrayList.toArray(new Stock[
+                    frontPalletArrayList.size()])));
+                marshalling.addPallet(new Pallet(backPalletArrayList.toArray(new Stock[
+                    backPalletArrayList.size()])));
             }
+            this.isActive = true;
+        } else {
+            throw new IllegalStateException("The Sequencer \"" + this.getName() + "\" is not " +
+                          "currently checked in as ready.");
         }
-        marshalling.clearStock();
-        while(pickingRequests.size() > 0 && pickingRequests.get(0).getStatus() == 3) {
-        System.out.println("Trying to complete PickingRequest.");
-            ArrayList<Stock> backPalletArrayList = new ArrayList<>();
-            ArrayList<Stock> frontPalletArrayList = new ArrayList<>();
-            Integer[] sequenceableSKUs = pickingRequests.remove(0).getSKUs();
-            boolean loadToFront = true;
-            ArrayList<Stock> sequenceableStock = marshalling.getSequenceableStock();
-            for(int skuIndex = 0; skuIndex < sequenceableSKUs.length; skuIndex++) {
-                boolean stockFound = false;
-                for(int stockIndex = 0; stockIndex < sequenceableStock.size() && 
-                    stockFound == false; stockIndex++) {
-                    if(sequenceableStock.get(stockIndex).getSKU().equals(
-                        sequenceableSKUs[skuIndex])) {
-                        stockFound = true;
-                        if(loadToFront == true) {
-                            loadToFront = false;
-                            frontPalletArrayList.add(marshalling.popSequenceableStock(
-                                stockIndex));
-                        } else {
-                            loadToFront = true;
-                            backPalletArrayList.add(marshalling.popSequenceableStock(stockIndex));
-                        }
-                    }
-                }
-            }
-            marshalling.addPallet(new Pallet(frontPalletArrayList.toArray(new 
-                                          Stock[frontPalletArrayList.size()])));
-            marshalling.addPallet(new Pallet(backPalletArrayList.toArray(new 
-                                          Stock[backPalletArrayList.size()])));
-        }
+    }
 
-        System.out.println("The sequencing was completed with the marshalling stock being: ");
-        for(int si = 0; si < stock.size(); si++) {
-            System.out.println(stock.get(si).getSKU());
-        }
-        System.out.println("The sequencing was completed with the marshalling sequenceable " +
-            "stock being: ");
-        for(int sj = 0; sj < marshalling.getSequenceableStock().size(); sj++) {
-            System.out.println(marshalling.getSequenceableStock().get(sj).getSKU());
-        }
-        System.out.println("The sequencing was completed with the marshalling pallets being: ");
-        for(int sk = 0; sk < marshalling.getMarshallingPallets().size(); sk++) {
-            System.out.println("    Pallet " + Integer.toString(sk) + " : ");
-            for(int sl = 0; sl < marshalling.getMarshallingPallets().get(sk).getContent().length;
-                sl++) {
-                System.out.println("        " + 
-                    marshalling.getMarshallingPallets().get(sk).getContent()[sl].getSKU());
-            }
+    public void setReady() {
+        if(this.isActive == true) {
+            this.isActive = false;
+        } else {
+            throw new IllegalStateException("The Sequencer \"" + this.getName() + "\" is " +
+                          "currently checked in as ready.");
         }
     }
 }
